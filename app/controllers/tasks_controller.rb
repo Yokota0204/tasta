d65e8cdf08fe3ea97b902c2c6d23f47fb2cdf937
Yokota0204 <yokota.02210301@gmail.com>
2019-08-20 13:35:33 +0900

    トップ画面、タスク一覧画面、タスク新規作成画面の画面表示を実装。

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,20 @@
+class TasksController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def edit
+    end
+
+    def delete
+        render :index
+    end
+end
